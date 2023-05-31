6ec924ec42848401da41b941057972e329010acb
SofMoquito <sofsaules@gmail.com>
2023-05-31 12:58:01 -0600

    modal de cada pokémon

--- src/main.js
+++ src/main.js
@@ -80,5 +80,73 @@ sortDropdown.addEventListener("change", () => {
   /*console.log(sortPokemon)*/
 });
 
+//Ventana modal
+const modal = document.querySelector("#modulos");
+for (let i = 0; i < data.pokemon.length; i++) {
+  const poke = data.pokemon[i];
+  showPokemon(poke);
+}
+
+function showPokemon(poke){
+  let tipos = poke.type.map((typeElement) => `<p class="tipo">${typeElement}</p>`);
+  tipos = tipos.join("-"); 
+  let debilidades = poke.weaknesses.map((weaknessesElement) => `<p class="tipo">${weaknessesElement}</p>`);
+  debilidades = debilidades.join("-"); 
+  let resistente = poke.resistant.map((resistantElement) => `<p class="tipo">${resistantElement}</p>`);
+ resistente = resistente.join("-"); 
+
+  const div = document.createElement("div");
+  div.classList.add("pokemonModulos");
+  div.innerHTML = `
+    <div class="encabezadoModulo">
+    <h2 class="pokemon-nombre">${poke.name}</h2>
+    <p class="pokemon-id">#${poke.num}</p>
+ </div>
+ <div class="containerAbout">
+   <div class="lateralIzquierdo">
+     <div class="pokemon-imagen-modulo">
+       <img src="${poke.img}" alt="Pokemon ${poke.name}">
+     </div>
+      <div class="pokemon-tipos">
+      ${tipos}
+      </div>
+   </div>
+ <div class="details">
+   <div class="parrafo">
+     <p>About</p>
+     <p>${poke.about}</p>
+   </div>
+     <div class="HW">
+       <div>
+         <p class="bold">Height</p>
+         <p>${poke.size.height}</p>
+       </div>   
+       <div>
+         <p class="bold">Weight</p>
+         <p>${poke.size.weight}</p>
+       </div> 
+     </div>  
+     </div>
+ </div>
+<div class="WR">
+   <div class="Weaknesses">
+     <p>Weaknesses</p>
+     <div class="W-types"> 
+     ${debilidades}
+     </div>
+   </div>
+   <div class="resistant">
+     <p>Resistant</p>
+     <div class="R-types">
+     ${resistente}
+  </div>
+ </div>
+ </div>
+ `;
+
+  modal.append(div); 
+
+}
+
 
 console.log(data);
